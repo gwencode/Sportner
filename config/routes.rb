e2c06674c3852c6f6ea2ab550384d3d0ba784d1c
Matthieu Nourry <nourrymatthieu@gmail.com>
2022-11-29 18:06:09 +0100

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "events/map", to: "events#map"
+
+  resources :events  do
+    resources :participation, only: :create
+    resources :reviews, only: :create
+
+  end
+
+  resources :profiles, only: %i[edit update]
+  resources :my_events, only: :index
+  post "events/:id/duplicate", to: "events#duplicate"
+  resources :itineratie, only: :create
+  resource :places, only: :index
 end
