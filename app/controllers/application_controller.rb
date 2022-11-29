2e3e00afc45e8f581698c9e9f51fd9e79d648cba
Gwendal Le Bris <gwen.coding@gmail.com>
2022-11-29 19:39:13 +0100

    change device controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, if: :devise_controller?
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
