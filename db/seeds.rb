51c09370410345cb322106ca12facca632f39de2
Olivier Kermoal <olivier.kermoal@gmail.com>
2022-11-29 13:08:35 +0100

    Create 7 profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "cleaning DB..."
+puts "destroying events..."
+Event.destroy_all
+puts "destroying reviews..."
+Review.destroy_all
+puts "destroying participations..."
+Participation.destroy_all
+puts "destroying favorite_spots..."
+FavoriteSpot.destroy_all
+puts "destroying itineraries..."
+Itinerary.destroy_all
+puts "destroying run_details..."
+RunDetail.destroy_all
+puts "destroying spots..."
+Spot.destroy_all
+puts "destroying meteos..."
+Meteo.destroy_all
+puts "destroying users..."
+User.destroy_all
+puts "...cleaning done!"
+
+puts "Create users..."
+
+users = []
+user_data = [
+  [{first_name: "Clément", last_name: "Cordeiro", email: "clement@me.com", password: "secret", runner: true, surfer: true, address: "9 avenue du président Edouard Heriot, 56000 Vannes", birthday: "26/11/1994"}]
+  [{first_name: "Gwendal", last_name: "Le Bris", email: "gwendal@me.com", password: "secret", runner: true, surfer: true, address: "135 rue la Marck, 75018 Paris", birthday: "12/06/1995"}]
+  [{first_name: "Matthieu", last_name: "Nourry", email: "matthieu@me.com", password: "secret", runner: true, surfer: false, address: "labidois, 35490 Romazy", birthday: "29/07/2000"}]
+  [{first_name: "Olivier", last_name: "Kermoal", email: "olivier@me.com", password: "secret", runner: true, surfer: false, address: "4 avenue de la mare Guesclin, 35230 Saint-Erblon", birthday: "17/09/1990"}]
+  [{first_name: "Steven", last_name: "Dorion", email: "steven@me.com", password: "secret", runner: false, surfer: true, address: "15 rue Vanneau, 35230 Orgères", birthday: "13/05/1995"}]
+  [{first_name: "Justine", last_name: "Brigand", email: "justine@me.com", password: "secret", runner: true, surfer: false, address: "5 Rès le Golfe, 56000 Vannes", birthday: "15/08/1989"}]
+  [{first_name: "Magalie", last_name: "Girard", email: "magalie@me.com", password: "secret", runner: false, surfer: true, address: "12 Rue Fallempin, 75015 Paris", birthday: "03/02/1981"}]
+]
+user_data.each do |u|
+  user = User.create(u.first)
+  # file = File.open("db/fixtures/#{u.last}")
+  # user.avatar.attach(io: file, filename: u.last)
+  # user.save!
+  users << user
+end
