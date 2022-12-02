1bb20c3d45b9d18b36d3187b4d7c76d453cc025c
Olivier Kermoal <olivier.kermoal@gmail.com>
2022-12-02 12:35:49 +0100

    Mis à jour des élements navbar top et bot ainsi que de l'affichage des pages pour qu'elles prennent bien tout l'écran

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,9 +20,17 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.organizer = current_user
+    if @event.save!
+      redirect_to event_path(@event), success: "Evenement créé 👍"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -35,4 +43,8 @@ class EventsController < ApplicationController
   end
 
   private
+
+  def event_params
+    params.require(:event).permit(:name, :description)
+  end
 end
