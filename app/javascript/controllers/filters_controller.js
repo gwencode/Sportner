2411e671cd7b9f53a325824c265264db6a30825e
Clement Cordeiro <clement.cordeiro@gmail.com>
2022-12-04 13:31:02 +0100

    trying to filtr with calendar

--- app/javascript/controllers/filters_controller.js
+++ app/javascript/controllers/filters_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["runbtn", "surfbtn", "runcard", "surfcard"]
+  static targets = ["runbtn", "surfbtn", "runcard", "surfcard", "rangedate"]
   connect() {
     console.log("Hello from filter controler")
     console.log(this.runcardTargets)
@@ -16,5 +16,18 @@ export default class extends Controller {
   surffilter() {
     this.surfbtnTarget.classList.toggle("select");
     this.surfcardTargets.forEach((target) => target.classList.toggle("d-none"));
+  }
+
+  datefilter() {
+    // console.log(this.rangedateTarget.value.split());
+    const dates = this.rangedateTarget.value.split();
+    console.log(dates);
+    // console.log(dates[0].split("-"));
+    // const startdate = dates[0].split("-");
+    // console.log(startdate)
+    // console.log(startdate.class)
+    // const enddate = dates.last.first.split("-");
+    // console.log(startdate);
+    // console.log(enddate);
   }
 }
