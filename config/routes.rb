0d50596bd5fe4032948cb4eae22a7ab7b5d03666
Gwendal Le Bris <gwen.coding@gmail.com>
2022-12-05 17:19:18 +0100

    change profiles into profile

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :reviews, only: :create
   end
 
-  resources :profiles, only: %i[show edit update]
+  resource :profile, only: %i[show edit update]
 
   resources :my_events, only: :index
   post "events/:id/duplicate", to: "events#duplicate"
