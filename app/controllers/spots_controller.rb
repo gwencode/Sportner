d3b93eaac2d15ca41b21defa46abf336968f955c
Clement Cordeiro <clement.cordeiro@gmail.com>
2022-12-05 17:15:05 +0100

    api meteo

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -5,6 +5,7 @@ class SpotsController < ApplicationController
 
   def show
     @spot = Spot.find(params[:id])
+    set_weather_data
   end
 
   def map
@@ -17,5 +18,34 @@ class SpotsController < ApplicationController
         image_url: helpers.asset_url("vague.png")
       }
     end
+  end
+
+  private
+
+  def set_weather_data
+    @weather_data = @spot.call_weather_api
+    if Time.now.hour >= 0 && Time.now.hour <= 9
+      @hourly_data = @weather_data[:data][:weather][0][:hourly].select { |i| i[:time] == "600" }.first
+    elsif Time.now.hour >= 10 && Time.now.hour <= 15
+      @hourly_data = @weather_data[:data][:weather][0][:hourly].select { |i| i[:time] == "1200" }.first
+    else
+      @hourly_data = @weather_data[:data][:weather][0][:hourly].select { |i| i[:time] == "1800" }.first
+    end
+    @temp_c = @hourly_data[:tempC]
+    @watertemp_c = @hourly_data[:waterTemp_C]
+    @windspeedkmph = @hourly_data[:windspeedKmph]
+    @winddirdegree = @hourly_data[:winddirDegree]
+    @winddir16point = @hourly_data[:winddir16Point]
+    @swellheight_m = @hourly_data[:swellHeight_m]
+    @swellperiod_secs = @hourly_data[:swellPeriod_secs]
+    @swelldir16point = @hourly_data[:swellDir16Point]
+
+    # @tidelow1 = @weather_data[:data][:weather][0][:tides][0][:tide_data][0][:tideDateTime]
+    @tide = [
+      [@weather_data[:data][:weather][0][:tides][0][:tide_data][0][:tideDateTime], @weather_data[:data][:weather][0][:tides][0][:tide_data][0][:tide_type]],
+      [@weather_data[:data][:weather][0][:tides][0][:tide_data][1][:tideDateTime], @weather_data[:data][:weather][0][:tides][0][:tide_data][1][:tide_type]],
+      [@weather_data[:data][:weather][0][:tides][0][:tide_data][2][:tideDateTime], @weather_data[:data][:weather][0][:tides][0][:tide_data][2][:tide_type]],
+      [@weather_data[:data][:weather][0][:tides][0][:tide_data][3][:tideDateTime], @weather_data[:data][:weather][0][:tides][0][:tide_data][3][:tide_type]]
+  ]
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  require "json"
+  require "open-uri"
   belongs_to :organizer, class_name: "User", foreign_key: :user_id
   belongs_to :spot, optional: true
   belongs_to :run_detail, optional: true
@@ -19,4 +21,10 @@ class Event < ApplicationRecord
   DIFFICULTIES = %i[débutant intermédiaire confirmé]
 
   has_many_attached :photos
+  
+  def call_weather_api
+    url = "https://api.worldweatheronline.com/premium/v1/marine.ashx?key=85ca93f406684910b42131430220512&q=#{self.latitude},#{self.longitude}&format=json&includelocation=yes&tide=yes&tp=6"
+    weather_serialized = URI.open(url).read
+    weather = JSON.parse(weather_serialized, symbolize_names: true)
+  end
 end
